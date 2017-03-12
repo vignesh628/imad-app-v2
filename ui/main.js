6e5dc675eba10149cb8e96ee7521136a800daa72
vignesh628 <vignesh943628@gmail.com>
2017-03-13 03:40:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,19 @@ img.onclick = function ()
 {
     var interval = setInterval(moveRight, 50);
 };
+var hi
+var marginLeft = 0;
+function moveRight()
+
+
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px'
+    
+    
+}
+
+img.onclick = function ()
+{
+    var interval = setInterval(moveRight, 50);
+};
