040146cb5240ea4b0110dd2f0f9794e0dd45f5c0
vignesh628 <vignesh943628@gmail.com>
2017-03-13 00:16:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.jss', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.jss'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
