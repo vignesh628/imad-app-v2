601d2c05767ce19605a87e80f93c6356874ff399
vignesh628 <vignesh943628@gmail.com>
2017-03-13 01:44:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+
+
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px'
+    
+    
+}
+
+img.onclick = function ()
+{
+    var interval = setinterval(moveRight,100);
+};
+
